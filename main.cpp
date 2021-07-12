27334a7d1d13a7ab3ce1ea985a8cb19acafb1749
mahmoudalfawair <84728207+mahmoudalfawair@users.noreply.github.com>
2021-07-12 23:55:57 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int x;
-   cin >> x;
-   int y;
-   cin >> y;
-   if(x>y)
-       cout << x << endl;
-   else if(x=y)
-       cout << x << " = " << y << endl;
-   else
-       cout << y <<endl;
-    return 0;
+  int grade;
+  cin >> grade;
+  if(grade >=0 && grade < 50)
+      cout << "F" << endl;
+  else if(grade >=50 && grade <60 )
+      cout << "D " << endl;
+  else if(grade >=60 && grade <70 )
+      cout << "C" << endl;
+  else if(grade >=70 && grade <80)
+      cout << "B" << endl;
+  else if(grade >=80 && grade <90 )
+      cout << "A" << endl;
+  else if(grade >=90 && grade <100 )
+      cout << "0" << endl;
+      return 0;
 }
